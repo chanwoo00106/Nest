f3d37e8f854f9884f9d9bb4460a6e4d5fc810cf2
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-11-04 19:03:49 +0900

    pipe 설정

--- mongo/src/blog/dto/blog.dto.ts
+++ mongo/src/blog/dto/blog.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class BlogDTO {
+  @IsString()
   readonly title: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString()
   readonly user: string;
 }
--- mongo/src/main.ts
+++ mongo/src/main.ts
@@ -1,9 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // 전역 pipe 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3300);
 }
 bootstrap();
