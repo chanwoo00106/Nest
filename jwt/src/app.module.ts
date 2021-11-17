d14d6711e3341f8802c43a279617747637e44ba2
변찬우 <chan00106@naver.com>
2021-11-17 20:11:21 +0900

    어려우니깐 다시

--- jwt/src/app.module.ts
+++ jwt/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
+  imports: [],
+  controllers: [],
 })
 export class AppModule {}
