4b5f4edb2ec5ec9f9c3a027bd41b650ecb829c31
변찬우 <chan00106@naver.com>
2021-11-17 18:03:33 +0900

    로그인 분석

--- jwt/src/app.module.ts
+++ jwt/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController],
 })
 export class AppModule {}
