f86ce80300c2aaaae81cef26706431cc6c849e5a
변찬우 <chan00106@naver.com>
2021-11-22 17:59:37 +0900

    이게 뭐지?

--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
