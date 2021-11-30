107698b144b6dc2b1515ceda07128aaf0bd23830
변찬우 <chan00106@naver.com>
2021-11-30 20:55:35 +0900

    뭐지..?

--- slack/src/auth/auth.service.ts
+++ slack/src/auth/auth.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Users } from '../entities/Users';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string): string {
-    return `${email} ${password}`;
+  constructor(
+    @InjectRepository(Users) private userRepository: Repository<Users>,
+  ) {}
+  async validateUser(email: string, password: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    console.log(email, password, user);
+    if (!user) {
+      return null;
+    }
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    }
+    return null;
   }
 }
