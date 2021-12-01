56bf96ebbc45271bd86df547f5e9cb04cccfecc5
변찬우 <chan00106@naver.com>
2021-12-01 20:15:09 +0900

    update service

--- slack/src/users/users.module.ts
+++ slack/src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users, WorkspaceMembers, ChannelMembers]),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
--- slack/src/users/users.service.ts
+++ slack/src/users/users.service.ts
@@ -3,11 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private userRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMebersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
   ) {}
   async join(email: string, nickname: string, password: string) {
     const user = await this.userRepository.findOne({ where: { email } });
@@ -15,10 +21,19 @@ export class UsersService {
       throw new UnauthorizedException();
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.userRepository.save({
+    const returned = await this.userRepository.save({
       email,
       nickname,
       password: hashedPassword,
     });
+    await this.workspaceMebersRepository.save({
+      UserId: returned.id,
+      WorkspaceId: 1,
+    });
+    await this.channelMembersRepository.save({
+      UserId: returned.id,
+      ChannelId: 1,
+    });
+    return true;
   }
 }
