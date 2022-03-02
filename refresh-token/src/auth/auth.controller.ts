74ada35c5501763e7af9ff78a75e86df2eec08fe
변찬우 <chan00106@naver.com>
2022-03-03 00:22:07 +0900

    Add refreshTokens

--- refresh-token/src/auth/auth.controller.ts
+++ refresh-token/src/auth/auth.controller.ts
@@ -34,13 +34,15 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   logout(@Req() req: Request) {
     const user = req.user;
-    return this.authService.logout(user['id']);
+    return this.authService.logout(user['sub']);
   }
 
   @UseGuards(AuthGuard('jwt-refresh'))
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
-  refreshTokens() {
-    // return this.authService.refreshTokens();
+  refreshTokens(@Req() req: Request) {
+    const user = req.user;
+    console.log(user);
+    return this.authService.refreshTokens(user['sub'], user['refreshToken']);
   }
 }
--- refresh-token/src/auth/auth.service.ts
+++ refresh-token/src/auth/auth.service.ts
@@ -56,7 +56,21 @@ export class AuthService {
     });
   }
 
-  refreshTokens() {}
+  async refreshTokens(userId: number, rt: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) throw new ForbiddenException('Access Denied');
+
+    const rtMatches = await bcrypt.compare(rt, user.hashedRt);
+    if (!rtMatches) throw new ForbiddenException('Access Denied');
+
+    const tokens = await this.getToken(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
 
   async updateRtHash(userId: number, rt: string) {
     const hash = await this.hashData(rt);
